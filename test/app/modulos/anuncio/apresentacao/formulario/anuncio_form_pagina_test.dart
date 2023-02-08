d1eca03e1ed2c4b101df6cf848232da14b7708d1
Evaldo <evaldo@parser.com.br>
2023-02-08 16:00:11 -0300

    .

--- test/app/modulos/anuncio/apresentacao/formulario/anuncio_form_pagina_test.dart
+++ test/app/modulos/anuncio/apresentacao/formulario/anuncio_form_pagina_test.dart
@@ -6,18 +6,20 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Anuncio Form valida titulo ...', (tester) async {
     var anuncio = Anuncio(
-      titulo: "",
-    );
-    final botaoFinder = find.byKey(const Key("btsalvar")); // localiza o botão
-    final validacaoFinder = find.text("Digite um título para o anúncio.");
+      titulo: "", // titulo vazio
+    ); // objeto novo
+    final botaoFinder =
+        find.byKey(const Key("btsalvar")); // localizador  do botão
+    final validacaoFinder = find.text(
+        "Digite um título para o anúncio."); // localizador da mensagem de validação
 
     await tester.pumpWidget(
-        MaterialApp(home: AnuncioFormPagina(anuncio))); // lança o widget
+        MaterialApp(home: AnuncioFormPagina(anuncio))); // abre o widget
     expect(botaoFinder, findsOneWidget); // verifica se o botão foi encontrado
-    await tester.tap(botaoFinder);
-    await tester.pumpAndSettle();
+    await tester.tap(botaoFinder); // pressiona o botão
+    await tester.pumpAndSettle(); // aguarda a interface atualizar
     expect(validacaoFinder, findsOneWidget,
         reason:
-            "formulário permitindo titulo vazio"); // verifica se o botão foi encontrado
+            "formulário permitindo titulo vazio"); // verifica se a validação apareceu
   });
 }
